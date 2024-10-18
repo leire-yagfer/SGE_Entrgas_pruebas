0444db03dab19bf7c90df79d617b037f21955285
leire-yagfer <leire.yagfer.1@educa.jcyl.es>
2024-10-18 14:17:09 +0200

    terminar http

--- SGE_Entrega12/proyecto.py
+++ SGE_Entrega12/proyecto.py
@@ -1,6 +1,7 @@
 import json
 import os
 import random
+import requests
 from collections import defaultdict
 
 class Palabra:
@@ -36,21 +37,20 @@ class Usuario:
     def guardar_vocabulario(self):
         try:
             with open(f'{self.nombre}_vocabulario.json', 'w') as f:
-                json.dump([vars(palabra) for palabra in self.vocabulario], f, indent=3)  # Usamos indent=4 para mejorar la legibilidad
+                json.dump([vars(palabra) for palabra in self.vocabulario], f, indent=3)
         except IOError as e:
             print(f"Error al guardar el vocabulario: {e}")
 
     def agregar_palabra(self, palabra):
-        # Verificamos si la palabra ya existe en el vocabulario
         if any(p.palabra_original.lower() == palabra.palabra_original.lower() for p in self.vocabulario):
             print("La palabra ya existe en su vocabulario.")
-            return  # Salimos si la palabra ya está
+            return
         else:
             self.vocabulario.append(palabra)
             self.guardar_vocabulario()
 
     def listar_vocabulario(self):
-        if not self.vocabulario:  # Verificamos si el vocabulario está vacío
+        if not self.vocabulario:
             print("Todavía no hay palabras en su vocabulario.")
         else:
             for palabra in self.vocabulario:
@@ -60,30 +60,26 @@ class Usuario:
         return [palabra for palabra in self.vocabulario if palabra.aprendida]
 
     def borrar_vocabulario(self):
-        """Borra todo el vocabulario del usuario, dejando el archivo vacío."""
         self.vocabulario = []
-        self.guardar_vocabulario()  # Guarda el estado vacío en el archivo
+        self.guardar_vocabulario()
         print("Todo el vocabulario ha sido borrado.")
 
 class Vocabulario:
     def __init__(self):
-        self.usuarios = {}  # Usamos un diccionario para almacenar instancias de Usuario con su nombre como clave
-        self.todas_palabras_usuarios = []  # Lista para almacenar todas las palabras de todos los usuarios
-        self.palabras_unicas = set()  # Set para almacenar palabras únicas
-        self.cargar_vocabulario_global()  # Cargar el vocabulario global al iniciar
+        self.usuarios = {}
+        self.todas_palabras_usuarios = []
+        self.palabras_unicas = set()
+        self.cargar_vocabulario_global()
 
     def cargar_vocabulario_global(self):
-        """Carga el vocabulario global desde un archivo JSON si existe."""
         if os.path.exists('vocabulario_global.json'):
             with open('vocabulario_global.json', 'r') as f:
                 self.todas_palabras_usuarios = json.load(f)
-                # Añadir palabras únicas al set
                 for palabra in self.todas_palabras_usuarios:
                     info_palabra = (palabra['palabra_original'].lower(), palabra['traduccion'], palabra['categoria'])
                     self.palabras_unicas.add(info_palabra)
 
     def guardar_vocabulario_global(self):
-        """Guarda el vocabulario global en un archivo JSON."""
         try:
             with open('vocabulario_global.json', 'w') as f:
                 json.dump(self.todas_palabras_usuarios, f, separators=(',', ':\n'))
@@ -94,28 +90,24 @@ class Vocabulario:
         self.usuarios[usuario.nombre] = usuario
 
     def agregar_palabra(self, palabra_agregar, traduccion, categoria, usuario):
-        # Usamos una tupla para almacenar la información de la palabra
         info_palabra = (palabra_agregar.lower(), traduccion, categoria)
 
-        # Verificamos si la palabra ya está en el vocabulario del usuario
         if any(p.palabra_original.lower() == palabra_agregar.lower() for p in usuario.vocabulario):
             print("La palabra ya existe en el vocabulario del usuario. Introduzca una nueva.")
-            return  # Salimos si la palabra ya está en el vocabulario del usuario
-        
-        # Crear nueva palabra
+            return
+
         palabra_nueva = Palabra(palabra_agregar, traduccion, categoria)
-        usuario.agregar_palabra(palabra_nueva)  # Intentamos agregar la palabra al vocabulario del usuario
-        
-        # Agregar la palabra al vocabulario global sin el estado de aprendizaje
+        usuario.agregar_palabra(palabra_nueva)
+
         if info_palabra not in self.palabras_unicas:
             self.todas_palabras_usuarios.append({
                 'palabra_original': palabra_agregar,
                 'traduccion': traduccion,
                 'categoria': categoria
-            })  # Agrega la información de la palabra al vocabulario global sin 'aprendida'
-            self.palabras_unicas.add(info_palabra)  # Agrega la palabra al set de palabras únicas
-            self.guardar_vocabulario_global()  # Guarda el vocabulario global actualizado
-            
+            })
+            self.palabras_unicas.add(info_palabra)
+            self.guardar_vocabulario_global()
+
         print("Palabra agregada con éxito.")
 
     def practicar_vocabulario(self, usuario):
@@ -132,39 +124,80 @@ class Vocabulario:
             print("No hay palabras en el vocabulario para practicar.")
 
     def listar_vocabularios_todos(self):
-        if not self.todas_palabras_usuarios:  # si no hay palabras globales
+        if not self.todas_palabras_usuarios:
             print("Todavía no hay ninguna palabra almacenada.")
-            return  # salgo de la función
+            return
 
-        palabras_mostradas = set()  # Usamos un set para almacenar las palabras originales que ya hemos mostrado
+        palabras_mostradas = set()
         for info in self.todas_palabras_usuarios:
-            if info['palabra_original'] not in palabras_mostradas:  # Si no hemos mostrado esta palabra
+            if info['palabra_original'] not in palabras_mostradas:
                 print(f"{info['palabra_original']} - {info['traduccion']} (Categoría: {info['categoria']})")
-                palabras_mostradas.add(info['palabra_original'])  # Añadimos la palabra al set para evitar repeticiones
+                palabras_mostradas.add(info['palabra_original'])
 
     def listar_vocabulario_por_categoria(self):
         if not self.todas_palabras_usuarios:
             print("No hay palabras almacenadas.")
             return
 
-        # Agrupamos palabras por categoría
         vocabulario_por_categoria = defaultdict(list)
         for palabra in self.todas_palabras_usuarios:
             vocabulario_por_categoria[palabra['categoria']].append(palabra)
 
-        # Ordenamos las categorías y las palabras en cada categoría
         for categoria in sorted(vocabulario_por_categoria.keys()):
             print(f"Categoría: {categoria}")
             for palabra in sorted(vocabulario_por_categoria[categoria], key=lambda x: x['palabra_original']):
                 print(f"  {palabra['palabra_original']} - {palabra['traduccion']}")
 
     def copiar_vocabulario_global_a_usuario(self, usuario):
-        """Copia palabras del vocabulario global al vocabulario del usuario si no existen ya."""
         for info in self.todas_palabras_usuarios:
             palabra_nueva = Palabra(info['palabra_original'], info['traduccion'], info['categoria'])
             if not any(p.palabra_original.lower() == palabra_nueva.palabra_original.lower() for p in usuario.vocabulario):
                 usuario.agregar_palabra(palabra_nueva)
                 print(f"Palabra '{palabra_nueva.palabra_original}' copiada al vocabulario de {usuario.nombre}.")
+
+
+
+
+
+
+                
+
+    def traducir_palabra_es_a_en(self, palabra_buscar):
+        palabra_buscar = input("Dime el nombre o número del POKEMON a buscar: ").lower()
+
+
+
+        api_key = palabra_buscar
+        respuesta = f"https://translation.googleapis.com/language/translate/v2?key={palabra_buscar}"
+
+        data = {
+            'q': palabra_buscar,
+            'target': 'en',
+            'source': 'es'
+        }
+
+        try:
+            response = requests.post(respuesta, data=data)
+            if response.status_code == 200:
+                result = response.json()
+                return result['data']['translations'][0]['translatedText']
+            else:
+                print(f"Error en la solicitud de traducción: {response.status_code}")
+                return None
+        except Exception as e:
+            print(f"Error al realizar la petición HTTP: {e}")
+            return None
+        
+
+
+
+
+
+
+
+
+
+
 
 def main():
     vocabulario = Vocabulario()
@@ -175,7 +208,6 @@ def main():
             print("¡Hasta luego!")
             break
 
-        # si el usuario ya existe, uso la instancia existente
         if nombre_usuario in vocabulario.usuarios:
             usuario = vocabulario.usuarios[nombre_usuario]
             print(f"Bienvenido de nuevo, {nombre_usuario}!")
@@ -184,10 +216,10 @@ def main():
             vocabulario.agregar_usuario(usuario)
 
         while True:
-            opcion = input(f"\n---- Menú Principal para {nombre_usuario}. Seleccione una opción: ---- \n 1. Agregar Palabra \n 2. Listar vocabulario de {nombre_usuario} \n 3. Practicar vocabulario de {nombre_usuario} \n 4. Mostrar un listado de todas las palabras del vocabulario (de todos los usuarios) \n 5. Listar vocabulario global por categoría \n 6. Copiar vocabulario global a mi vocabulario \n 7. Borrar todo el vocabulario de {nombre_usuario} \n 8. Cambiar usuario \n 9. Salir \n")   
+            opcion = input(f"\n---- Menú Principal para {nombre_usuario}. Seleccione una opción: ---- \n 1. Agregar Palabra \n 2. Listar vocabulario de {nombre_usuario} \n 3. Practicar vocabulario de {nombre_usuario} \n 4. Mostrar un listado de todas las palabras del vocabulario (de todos los usuarios) \n 5. Listar vocabulario global por categoría \n 6. Copiar vocabulario global a mi vocabulario \n 7. Borrar todo el vocabulario de {nombre_usuario} \n 8. Cambiar usuario \n 9. Salir \n 10. Traducir palabra de español a inglés \n")
             
-            if opcion not in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
-                print("Opción no válida. Por favor, seleccione una opción del 1 al 9.")
+            if opcion not in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']:
+                print("Opción no válida. Por favor, seleccione una opción del 1 al 10.")
                 continue
 
             match opcion:
@@ -221,13 +253,16 @@ def main():
 
                 case "8":
                     print("Cambiando de usuario...")
-                    break  # sale del bucle interno (menú principal) para elegir otro usuario
+                    break
 
                 case "9":
                     print("Saliendo...")
-                    return  # finalizo el programa
+                    return
 
-                case _:
-                    print("Opción no válida. Por favor, seleccione una opción del 1 al 9.")
-
-main()
+                case "10":
+                    palabra_espanol = input("Ingrese la palabra en español que desea traducir al inglés: ")
+                    traduccion = vocabulario.traducir_palabra_es_a_en(palabra_espanol)
+                    if traduccion:
+                        print(f"La traducción de '{palabra_espanol}' es: {traduccion}")
+                    else:
+                        print("No se pudo obtener la traducción.")
